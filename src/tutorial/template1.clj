b4f69a279473c75244ad158e36c0a435c394e3a5
Sebastian Bensusan <sbensu@gmail.com>
2014-08-14 15:09:52 +0200

    maybe-content added in template1.clj to utils :refer

--- src/tutorial/template1.clj
+++ src/tutorial/template1.clj
@@ -1,7 +1,7 @@
 (ns tutorial.template1
   (:require [net.cgrand.enlive-html :as html])
   (:use [net.cgrand.moustache :only [app]]
-        [tutorial.utils :only [run-server render-to-response]]))
+        [tutorial.utils :only [run-server render-to-response maybe-content]]))
 
 (html/deftemplate index "tutorial/template1.html"
   [ctxt]
@@ -9,8 +9,8 @@
 
 ;; ========================================
 ;; The App
-;; ========================================
-
+;; =======================================
+=
 (def routes
   (app
    [""]       (fn [req] (render-to-response
